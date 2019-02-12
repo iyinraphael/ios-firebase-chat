c793971645be7a7e0d6bea01987276eb85e51f0c
iyinraphael <iyinraphael@gmail.com>
2019-02-12 14:25:21 -0800

    storyboard set up and outlets connected

---
+++ Firebase-Chat/Firebase-Chat/View Controller/MessageTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MessageTableViewCell.swift
+//  Firebase-Chat
+//
+//  Created by Iyin Raphael on 2/12/19.
+//  Copyright © 2019 Iyin Raphael. All rights reserved.
+//
+
+import UIKit
+
+class MessageTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+}
